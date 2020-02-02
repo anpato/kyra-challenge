38a3f618f1557f7dd614daf8b9ef8ff59d89feb0
Andre Pato <anpato1994@gmail.com>
2020-02-02 04:38:49 -0500

    add change check to document to only call insertions

--- middleware/WatchCollection.js
+++ middleware/WatchCollection.js
@@ -3,10 +3,13 @@ const { Video } = require('../db/Schema')
 module.exports = (req, res, next) => {
   const stream = Video.watch()
   stream.on('change', change => {
-    let uploads = []
-    const document = change.fullDocument
-    uploads.push(document)
-    res.locals.video = uploads
-    next()
+    if (change.operationType === 'insert') {
+      let uploads = []
+      const document = change.fullDocument
+      uploads.push(document)
+      res.locals.video = uploads
+      next()
+    }
+    res.send({ message: 'Only Insertions Supported' })
   })
 }
