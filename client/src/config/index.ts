f1e6a62c72cf4bb7123cfc218e46cad40a8c5d3d
Andre Pato <anpato1994@gmail.com>
2020-02-02 04:26:03 -0500

    add deploy url to axios config

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,7 +1,10 @@
 import Axios from 'axios'
 
 export const Api = Axios.create({
-  baseURL: `http://localhost:3001/api`,
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://kyra-challenge.herokuapp.com/api'
+      : `http://localhost:3001/api`,
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
